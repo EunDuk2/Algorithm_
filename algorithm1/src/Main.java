67b5c9154f38d40cd2e148fe53c2dc55513da34f
EunDuk2 <dmsqls6789@naver.com>
2025-09-24 13:42:51 +0900

    백준-28279(덱 2)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,22 +5,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+        Deque<Integer> deque = new ArrayDeque();
+        StringBuilder sb = new StringBuilder();
 
-        int total = 0;
-        Set<String> set = new HashSet();
+        for(int i = 0 ; i < N ; i++) {
+            String[] input = br.readLine().split(" ");
 
-        for(int i = 0 ; i < n ; i++) {
-            set.add(br.readLine());
-        }
-        for(int i = 0 ; i < m ; i++) {
-            if(set.contains(br.readLine())) {
-                total += 1;
+            int order = Integer.parseInt(input[0]);
+
+            if(order == 1) {
+                int num = Integer.parseInt(input[1]);
+                deque.addFirst(num);
+            } else if(order == 2) {
+                int num = Integer.parseInt(input[1]);
+                deque.addLast(num);
+            } else if(order == 3) {
+                sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+            } else if(order == 4) {
+                sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+            } else if(order == 5) {
+                sb.append(deque.size()).append("\n");
+            } else if(order == 6) {
+                sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+            } else if(order == 7) {
+                sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+            } else {
+                sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
             }
         }
-        System.out.println(total);
+        System.out.println(sb);
     }
 }

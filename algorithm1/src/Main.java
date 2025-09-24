13296902e11526670d3edddd4c7f450f17b65723
EunDuk2 <dmsqls6789@naver.com>
2025-09-24 20:01:47 +0900

    백준-11652(카드)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,36 +5,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = br.readLine().split(" ");
-
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(br.readLine());
 
         // 1
-        Map<String, Integer> map = new HashMap<>();
+        Map<Long, Integer> map = new HashMap();
+
         for(int i = 0 ; i < N ; i++) {
-            String key = br.readLine();
-            if(key.length() >= M) {
-                map.put(key, map.getOrDefault(key, 0)+1);
-            }
+            Long key = Long.parseLong(br.readLine());
+            map.put(key, map.getOrDefault(key, 0)+1);
         }
 
         // 2
-        List<String> list = new ArrayList(map.keySet());
+        List<Long> list = new ArrayList(map.keySet());
+
         list.sort((a, b) -> {
-            int compare1 = map.get(b) - map.get(a);
-            if(compare1 != 0) return compare1;
+           int compare = map.get(b) - map.get(a);
+           if(compare != 0) return compare;
 
-            int compare2 = b.length() - a.length();
-            if(compare2 != 0) return compare2;
-
-            return a.compareTo(b);
+           return Long.compare(a, b);
         });
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < list.size() ; i++) {
-            sb.append(list.get(i)).append("\n");
-        }
-        System.out.println(sb);
+        // 3
+        System.out.println(list.get(0));
     }
 }

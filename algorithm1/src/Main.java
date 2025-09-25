77c6ba39da769c23a08eeec031d133e894252089
EunDuk2 <dmsqls6789@naver.com>
2025-09-25 19:42:57 +0900

    백준-15650(N과 M2)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,30 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static List<List<Integer>> doubleList = new ArrayList();
+    static List<Integer> temp = new ArrayList();
+    static int N;
+    static int M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        String[] NM = br.readLine().split(" ");
+
+        N = Integer.parseInt(NM[0]);
+        M = Integer.parseInt(NM[1]);
 
         // 1
-        Map<Long, Integer> map = new HashMap();
-
-        for(int i = 0 ; i < N ; i++) {
-            Long key = Long.parseLong(br.readLine());
-            map.put(key, map.getOrDefault(key, 0)+1);
-        }
+        dfs(1);
 
         // 2
-        List<Long> list = new ArrayList(map.keySet());
-
-        list.sort((a, b) -> {
-           int compare = map.get(b) - map.get(a);
-           if(compare != 0) return compare;
-
-           return Long.compare(a, b);
-        });
-
-        // 3
-        System.out.println(list.get(0));
+        StringBuilder sb = new StringBuilder();
+        for(List<Integer> list : doubleList) {
+            for(int i = 0 ; i < list.size() ; i++) {
+                sb.append(list.get(i)).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void dfs(int start) {
+        if(temp.size() == M) {
+            doubleList.add(new ArrayList(temp));
+            return;
+        }
+        for(int i = start ; i <= N ; i++) {
+            temp.add(i);
+            dfs(i+1);
+            temp.remove(temp.size()-1);
+        }
     }
 }

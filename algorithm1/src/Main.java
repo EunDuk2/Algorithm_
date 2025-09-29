b399532946d7b046e79f05bc64ec04f73f35d89e
EunDuk2 <dmsqls6789@naver.com>
2025-09-30 00:17:15 +0900

    백준-1021(회전하는 큐)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,31 +5,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        String[] NM = br.readLine().split(" ");
+
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
 
         // 1
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            int compare = Math.abs(a) - Math.abs(b);
-            if(compare != 0) return compare;
-            return a - b;
-        });
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = 1 ; i <= N ; i++) {
+            dq.addLast(i);
+        }
+        String[] aArr = br.readLine().split(" ");
 
         // 2
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < N ; i++) {
-            int x = Integer.parseInt(br.readLine());
+        int min = 0;
+        for(int i = 0 ; i < M ; i++) {
+            int a = Integer.parseInt(aArr[i]);
 
-            if(x == 0) {
-                if(pq.isEmpty()) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(pq.poll()).append("\n");
-                }
+            int count1 = 0;
+            Deque<Integer> dq1 = new ArrayDeque<>(dq);
+            int count2 = 0;
+            Deque<Integer> dq2 = new ArrayDeque<>(dq);
+
+            // 2번
+            while(dq1.peekFirst() != a) {
+                dq1.addFirst(dq1.pollLast());
+                count1++;
+            }
+            // 3번
+            while(dq2.peekFirst() != a) {
+                dq2.addLast(dq2.pollFirst());
+                count2++;
+            }
+
+            if(count1 < count2) {
+                dq = dq1;
+                dq.pollFirst();
+                min += count1;
             } else {
-                pq.add(x);
+                dq = dq2;
+                dq.pollFirst();
+                min += count2;
             }
         }
-        
-        System.out.println(sb);
+
+        System.out.println(min);
     }
 }

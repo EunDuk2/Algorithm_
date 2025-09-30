666c00320966f56ab03fc64bae9f63e156e11a67
EunDuk2 <dmsqls6789@naver.com>
2025-09-30 15:04:52 +0900

    백준-9024(두 수의 합)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,38 +5,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] NM = br.readLine().split(" ");
-
-        int N = Integer.parseInt(NM[0]);
-        int M = Integer.parseInt(NM[1]);
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         // 1
-        LinkedList<Integer> q = new LinkedList<>();
-        for(int i = 1 ; i <= N ; i++) {
-            q.add(i);
-        }
-        String[] aArr = br.readLine().split(" ");
+        for(int i = 0 ; i < t ; i++) {
+            String[] nk = br.readLine().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
 
-        // 2
-        int min = 0;
-        for(int i = 0 ; i < M ; i++) {
-            int a = Integer.parseInt(aArr[i]);
-
-            if(q.indexOf(a) <= q.size() / 2) {
-                // 2번
-                while(q.peekFirst() != a) {
-                    q.addLast(q.removeFirst());
-                    min++;
-                }
-            } else {
-                // 3번
-                while(q.peekFirst() != a) {
-                    q.addFirst(q.removeLast());
-                    min++;
-                }
+            // 2
+            String[] nums = br.readLine().split(" ");
+            int[] list = new int[n];
+            for (int j = 0; j < n; j++) {
+                list[j] = Integer.parseInt(nums[j]);
             }
-            q.pollFirst();
+            Arrays.sort(list);
+
+            // 3
+            int left = 0;
+            int right = list.length-1;
+
+            int min = Integer.MAX_VALUE;
+            int count = 0;
+
+            while(left < right) {
+                int sum = list[left] + list[right];
+                int diff = Math.abs(sum-k);
+
+                if(diff == min) {
+                    count++;
+                } else if(diff < min) {
+                    min = diff;
+                    count = 1;
+                }
+
+                if(sum > k) right--;
+                else left++;
+            }
+            sb.append(count).append("\n");
         }
-        System.out.println(min);
+        System.out.println(sb);
     }
 }

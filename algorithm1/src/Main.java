3d4fc80f4a866093319114bc8683ac3ec5e1f93f
EunDuk2 <dmsqls6789@naver.com>
2025-10-03 15:40:18 +0900

    백준-24444(알고리즘수업-너비 우선 탐색 1)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,49 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int N, M, R;
+    static List<List<Integer>> doubleList = new ArrayList();
+    static boolean[] visited;
+    static int[] order;
+    static int cnt = 1;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NMR = br.readLine().split(" ");
 
-        int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(NMR[0]);
+        int M = Integer.parseInt(NMR[1]);
+        int R = Integer.parseInt(NMR[2]);
 
-        // 1
-        for(int i = 0 ; i < t ; i++) {
-            String[] nk = br.readLine().split(" ");
-            int n = Integer.parseInt(nk[0]);
-            int k = Integer.parseInt(nk[1]);
+        visited = new boolean[N+1];
+        order = new int[N+1];
 
-            // 2
-            String[] nums = br.readLine().split(" ");
-            int[] list = new int[n];
-            for (int j = 0; j < n; j++) {
-                list[j] = Integer.parseInt(nums[j]);
-            }
-            Arrays.sort(list);
-
-            // 3
-            int left = 0;
-            int right = list.length-1;
-
-            int min = Integer.MAX_VALUE;
-            int count = 0;
-
-            while(left < right) {
-                int sum = list[left] + list[right];
-                int diff = Math.abs(sum-k);
-
-                if(diff == min) {
-                    count++;
-                } else if(diff < min) {
-                    min = diff;
-                    count = 1;
-                }
-
-                if(sum > k) right--;
-                else left++;
-            }
-            sb.append(count).append("\n");
+        for(int i = 0 ; i < N+1 ; i++) {
+            doubleList.add(new ArrayList());
         }
+
+        for(int i = 0 ; i < M ; i++) {
+            String[] uv = br.readLine().split(" ");
+            int u = Integer.parseInt(uv[0]);
+            int v = Integer.parseInt(uv[1]);
+
+            doubleList.get(u).add(v);
+            doubleList.get(v).add(u);
+        }
+
+        for(int i = 0 ; i < doubleList.size() ; i++) {
+            Collections.sort(doubleList.get(i));
+        }
+
+        bfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1 ; i < order.length ; i++) {
+            sb.append(order[i]).append("\n");
+        }
+
         System.out.println(sb);
+    }
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList();
+        q.add(start);
+        visited[start] = true;
+        order[start] = cnt++;
+
+        while(!q.isEmpty()) {
+            int target = q.poll();
+            for(int next : doubleList.get(target)) {
+                if(!visited[next]) {
+                    q.add(next);
+                    visited[next] = true;
+                    order[next] = cnt++;
+                }
+            }
+        }
     }
 }

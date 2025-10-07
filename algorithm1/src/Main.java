ce40ac98c1a8626a18154d97017b76dde673f389
EunDuk2 <dmsqls6789@naver.com>
2025-10-08 01:40:36 +0900

    백준-2468(안전 영역) 다시 푼 버전

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -3,48 +3,44 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] doubleList;
+    static int[][] graph;
     static boolean[][] visited;
-    static int result = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int result = 1;
     static int cnt = 0;
-    static int maxHeight;
-    static List<int[]> temp = new ArrayList();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
 
-        temp.add(new int[]{0, 1});
-        temp.add(new int[]{1, 0});
-        temp.add(new int[]{-1, 0});
-        temp.add(new int[]{0, -1});
-
-        doubleList = new int[N][N];
+        graph = new int[N][N];
         visited = new boolean[N][N];
 
+        int maxHeight = 1;
         for(int i = 0 ; i < N ; i++) {
-            String[] input = br.readLine().split(" ");
+            String[] line = br.readLine().split(" ");
             for(int j = 0 ; j < N ; j++) {
-                doubleList[i][j] = Integer.parseInt(input[j]);
-                if(maxHeight < doubleList[i][j]) maxHeight = doubleList[i][j];
+                int num = Integer.parseInt(line[j]);
+                graph[i][j] = num;
+                if(graph[i][j] > maxHeight) maxHeight = graph[i][j];
             }
         }
 
         for(int h = 1 ; h <= maxHeight ; h++) {
-            for(int i = 0 ; i < N ; i++) {
-                for(int j = 0 ; j < N ; j++) {
-                    if(!visited[i][j] && doubleList[i][j] > h) {
-                        bfs(i, j, h);
+            for(int x = 0 ; x < N ; x++) {
+                for(int y = 0 ; y < N ; y++) {
+                    if(graph[x][y] > h && !visited[x][y]) {
+                        bfs(x, y, h);
                     }
                 }
             }
-            if(cnt > result) {
-                result = cnt;
-            }
             visited = new boolean[N][N];
+            result = Math.max(result, cnt);
             cnt = 0;
         }
-        System.out.println(result == 0 ? 1 : result);
+        System.out.println(result);
     }
     static void bfs(int x, int y, int h) {
         Queue<int[]> q = new LinkedList();
@@ -53,16 +49,12 @@ public class Main {
         cnt++;
         while(!q.isEmpty()) {
             int[] current = q.poll();
-
-            for(int i = 0 ; i < temp.size() ; i++) {
-                int a = current[0] + temp.get(i)[0];
-                int b = current[1] + temp.get(i)[1];
-
-                if(a >= 0 && a < N && b >= 0 && b < N) {
-                    if(!visited[a][b] && doubleList[a][b] > h) {
-                        q.add(new int[]{a, b});
-                        visited[a][b] = true;
-                    }
+            for(int i = 0 ; i < 4 ; i++) {
+                int curX = current[0] + dx[i];
+                int curY = current[1] + dy[i];
+                if(curX >= 0 && curX < N && curY >= 0 && curY < N && graph[curX][curY] > h && !visited[curX][curY]) {
+                    q.add(new int[]{curX, curY});
+                    visited[curX][curY] = true;
                 }
             }
         }

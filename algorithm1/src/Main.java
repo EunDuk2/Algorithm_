6aa34be39972c4412e21d5be20d7c235d698e596
EunDuk2 <dmsqls6789@naver.com>
2025-10-10 04:28:14 +0900

    백준-1918(후위연산식) 다시

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,53 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int I;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = {-2, -1, 1, 2, -2, -1, 1, 2};
-    static int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
-    static int[][] dist;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
 
-        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
-        for(int t = 0; t < T ; t++) {
-            I = Integer.parseInt(br.readLine());
-            graph = new int[I][I];
-            visited = new boolean[I][I];
-            dist = new int[I][I];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-            String[] currentXY = br.readLine().split(" ");
-            int currentX = Integer.parseInt(currentXY[0]);
-            int currentY = Integer.parseInt(currentXY[1]);
+            if (c >= 'A' && c <= 'Z') {
+                sb.append(c);
+            }
 
-            String[] targetXY = br.readLine().split(" ");
-            int targetX = Integer.parseInt(targetXY[0]);
-            int targetY = Integer.parseInt(targetXY[1]);
+            else if (c == '(') {
+                stack.push(c);
+            }
 
-            bfs(currentX, currentY, new int[]{targetX, targetY});
-
-            System.out.println(dist[targetX][targetY]);
-        }
-    }
-    static void bfs(int x, int y, int[] target) {
-        Queue<int[]> q = new LinkedList();
-        q.add(new int[]{x, y});
-        visited[x][y] = true;
-        dist[x][y] = 0;
-        while(!q.isEmpty()) {
-            int[] currentXY = q.poll();
-            for(int i = 0 ; i < 8 ; i++) {
-                int currentX = currentXY[0] + dx[i];
-                int currentY = currentXY[1] + dy[i];
-                if(currentX >= 0 && currentX < I && currentY >= 0 && currentY < I && !visited[currentX][currentY]) {
-                    q.add(new int[]{currentX, currentY});
-                    visited[currentX][currentY] = true;
-                    dist[currentX][currentY] = dist[currentXY[0]][currentXY[1]] + 1;
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
                 }
+                stack.pop();
+            }
+
+            else {
+                while (!stack.isEmpty() && priority(stack.peek()) >= priority(c)) {
+                    sb.append(stack.pop());
+                }
+                stack.push(c);
             }
         }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static int priority(char op) {
+        if (op == '(') return 0;
+        else if (op == '+' || op == '-') return 1;
+        else return 2;
     }
 }

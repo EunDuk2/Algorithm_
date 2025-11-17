c3f9b6c88db21d6528f0aa3f49deda0a0d677f7a
EunDuk2 <dmsqls6789@naver.com>
2025-11-17 21:28:45 +0900

    인프런-1.문자 찾기

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,28 +5,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
+        String str = br.readLine().toUpperCase();
+        Character ch = Character.toUpperCase(br.readLine().charAt(0));
 
-        List<Integer> A = new ArrayList<>();
-        for(int i = 0 ; i < N ; i++) {
-            A.add(Integer.parseInt(input[i]));
+        int result = 0;
+
+        for(int i = 0 ; i < str.length() ; i++) {
+            Character c = str.charAt(i);
+
+            if(c.equals(ch)) {
+                result += 1;
+            }
         }
-
-        int[] answer = new int[N];
-
-        int index = 0;
-        while(index != N) {
-            int minIndex = A.indexOf(Collections.min(A));
-            answer[minIndex] = index++;
-            A.set(minIndex, Integer.MAX_VALUE);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int a : answer) {
-            sb.append(a).append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(result);
     }
 }

4d114c75f567ae6ea298cda1e533ca8dd1feb9a2
EunDuk2 <dmsqls6789@naver.com>
2025-11-18 19:58:33 +0900

    인프런-2-7. 점수계산

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -12,28 +12,15 @@ public class Main {
             input[i] = Integer.parseInt(strArr[i]);
         }
 
-        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+        int score = 1;
         for(int i = 0 ; i < N ; i++) {
-            int reverseNum = changeReverse(input[i]);
-            if(isPrime(reverseNum)) sb.append(reverseNum).append(" ");
+            if(input[i] == 1) {
+                sum += score++;
+            } else {
+                score = 1;
+            }
         }
-        System.out.println(sb);
-    }
-    static int changeReverse(int num) {
-        int temp = num;
-        int reverse = 0;
-        while(temp > 0) {
-            int t = temp % 10;
-            reverse = reverse * 10 + t;
-            temp = temp / 10;
-        }
-        return reverse;
-    }
-    static boolean isPrime(int num) {
-        if(num == 1 || num == 0) return false;
-        for(int i = 2 ; i*i <= num ; i++) {
-            if(num % i == 0) return false;
-        }
-        return true;
+        System.out.println(sum);
     }
 }

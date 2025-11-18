92c23e539a0b1f449e7ae54c446946ca8389aa2b
EunDuk2 <dmsqls6789@naver.com>
2025-11-19 03:43:06 +0900

    백준-N과 M (5) - 15654

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,43 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int M;
+
+    static List<Integer> numbers = new ArrayList();
+    static List<List<Integer>> doubleList = new ArrayList();
+    static List<Integer> temp = new ArrayList();
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][5];
+        String[] NM = br.readLine().split(" ");
+        N = Integer.parseInt(NM[0]);
+        M = Integer.parseInt(NM[1]);
+        visited = new boolean[N];
 
-        for (int i = 0; i < N; i++) {
-            String[] tmp = br.readLine().split(" ");
-            for (int j = 0; j < 5; j++) {
-                arr[i][j] = Integer.parseInt(tmp[j]);
+        String[] inputNumbers = br.readLine().split(" ");
+        for(int i = 0 ; i < inputNumbers.length ; i++) {
+            numbers.add(Integer.parseInt(inputNumbers[i]));
+        }
+        numbers.sort(Comparator.naturalOrder());
+
+        dfs(0);
+
+        for (List<Integer> list : doubleList) {
+            for (int n : list) System.out.print(n + " ");
+            System.out.println();
+        }
+    }
+    static void dfs(int depth) {
+        if(temp.size() == M) {
+            doubleList.add(new ArrayList(temp));
+            return;
+        }
+        for(int i = 0 ; i < numbers.size() ; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                temp.add(numbers.get(i));
+                dfs(depth+1);
+                visited[i] = false;
+                temp.remove(temp.size()-1);
             }
         }
-
-        Map<Integer, Integer> map = new HashMap();
-
-        // 학생별로 반복
-        for(int i = 0 ; i < N ; i++) {
-            // 비교할 학생별로 반복
-            for(int j = i+1 ; j < N ; j++) {
-                // 5개 학년 비교
-                for(int k = 0 ; k < 5 ; k++) {
-                    if(arr[i][k] == arr[j][k]) {
-                        map.put(i+1, map.getOrDefault(i+1, 0)+1);
-                        map.put(j+1, map.getOrDefault(j+1, 0)+1);
-                        break;
-                    }
-                }
-            }
-        }
-        int maxStudent = 1;
-        int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < N ; i++) {
-            if(map.get(i+1) != null && map.get(i+1) > max) {
-                maxStudent = i+1;
-                max = map.get(i+1);
-            }
-        }
-        System.out.println(maxStudent);
     }
 }

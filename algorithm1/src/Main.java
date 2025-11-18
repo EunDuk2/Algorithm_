6a3c1eca9ea5f419f0257bf1fd57f861efcb8720
EunDuk2 <dmsqls6789@naver.com>
2025-11-18 17:09:40 +0900

    인프런-2-3. 가위 바위 보

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,22 +5,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        String[] strArr = br.readLine().split(" ");
-        int[] input = new int[N];
+        String[] strArrA = br.readLine().split(" ");
+        String[] strArrB = br.readLine().split(" ");
+        int[] A = new int[N];
+        int[] B = new int[N];
 
         for(int i = 0 ; i < N ; i++) {
-            input[i] = Integer.parseInt(strArr[i]);
+            A[i] = Integer.parseInt(strArrA[i]);
         }
-
-        int count = 0;
-        int height = 0;
+        for(int i = 0 ; i < N ; i++) {
+            B[i] = Integer.parseInt(strArrB[i]);
+        }
 
         for(int i = 0 ; i < N ; i++) {
-            if(input[i] > height) {
-                count++;
-                height = input[i];
-            }
+            int currentA = A[i];
+            int currentB = B[i];
+
+            if(currentA == currentB) System.out.println("D");
+            else if(currentA == 1 && currentB == 3) System.out.println("A");
+            else if(currentA == 2 && currentB == 1) System.out.println("A");
+            else if(currentA == 3 && currentB == 2) System.out.println("A");
+            else System.out.println("B");
         }
-        System.out.println(count);
     }
 }

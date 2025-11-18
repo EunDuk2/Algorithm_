d5401e1a39ad0bb07b80b5f055564feb25bab96c
EunDuk2 <dmsqls6789@naver.com>
2025-11-18 17:17:43 +0900

    인프런-2-4. 피보나치 수열

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -1,31 +1,25 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        String[] strArrA = br.readLine().split(" ");
-        String[] strArrB = br.readLine().split(" ");
-        int[] A = new int[N];
-        int[] B = new int[N];
 
-        for(int i = 0 ; i < N ; i++) {
-            A[i] = Integer.parseInt(strArrA[i]);
-        }
-        for(int i = 0 ; i < N ; i++) {
-            B[i] = Integer.parseInt(strArrB[i]);
+        int[] dp = new int[N+1];
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3 ; i < dp.length ; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        for(int i = 0 ; i < N ; i++) {
-            int currentA = A[i];
-            int currentB = B[i];
-
-            if(currentA == currentB) System.out.println("D");
-            else if(currentA == 1 && currentB == 3) System.out.println("A");
-            else if(currentA == 2 && currentB == 1) System.out.println("A");
-            else if(currentA == 3 && currentB == 2) System.out.println("A");
-            else System.out.println("B");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < dp.length ; i++) {
+            sb.append(dp[i]).append(" ");
         }
+        System.out.println(sb);
     }
 }

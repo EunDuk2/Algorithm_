30cddb4c749077d5b649bb57967af9525e990dbb
EunDuk2 <dmsqls6789@naver.com>
2025-11-18 14:25:38 +0900

    인프런-11. 문자열 압축

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -4,34 +4,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] strArr = br.readLine().split(" ");
-        char[] s = strArr[0].toCharArray();
-        char t = strArr[1].charAt(0);
+        char[] charArr = br.readLine().toCharArray();
 
-        int[] result = new int[s.length];
+        char temp = charArr[0];
+        int count = 0;
 
-        int distance = 1000;
-        for(int i = 0 ; i < s.length ; i++) {
-            if(s[i] == t) {
-                distance = 0;
-            } else {
-                distance++;
-            }
-            result[i] = distance;
-        }
-        distance = 1000;
-        for(int i = s.length-1 ; i >= 0 ; i--) {
-            if(s[i] == t) {
-                distance = 0;
-            } else {
-                distance++;
-            }
-            result[i] = Math.min(result[i], distance);
-        }
         StringBuilder sb = new StringBuilder();
-        for(int n : result) {
-            sb.append(n).append(" ");
+        for(char c : charArr) {
+            if(temp == c) {
+                count++;
+            } else {
+                sb.append(temp);
+                if(count != 1) sb.append(count);
+                temp = c;
+                count = 1;
+            }
         }
+        sb.append(temp);
+        if(count != 1) sb.append(count);
         System.out.println(sb);
     }
 }

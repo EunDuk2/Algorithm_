c2ec6e37854c35ddc98f05ed457330fa22bc844b
EunDuk2 <dmsqls6789@naver.com>
2025-11-18 20:29:37 +0900

    인프런-2-8. 등수구하기

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -7,20 +7,27 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] strArr = br.readLine().split(" ");
+
         int[] input = new int[N];
+        List<Integer> list = new ArrayList();
         for(int i = 0 ; i < N ; i++) {
             input[i] = Integer.parseInt(strArr[i]);
+            list.add(input[i]);
         }
 
-        int sum = 0;
-        int score = 1;
-        for(int i = 0 ; i < N ; i++) {
-            if(input[i] == 1) {
-                sum += score++;
-            } else {
-                score = 1;
-            }
+        list.sort(Comparator.reverseOrder());
+
+        Map<Integer, Integer> map = new HashMap();
+        int rank = 1;
+        for(int score : list) {
+            if(!map.containsKey(score)) map.put(score, rank);
+            rank++;
         }
-        System.out.println(sum);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < N ; i++) {
+            sb.append(map.get(input[i])).append(" ");
+        }
+        System.out.println(sb);
     }
 }

6e9897606ef7a78250a6cdb0e8082f02cda8f487
EunDuk2 <dmsqls6789@naver.com>
2025-11-20 03:17:00 +0900

    인프런-3-6. 최대 길이 연속부분수열

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -4,27 +4,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int K = Integer.parseInt(NM[1]);
 
+        String[] inputStr = br.readLine().split(" ");
         int[] numbers = new int[N];
 
         for(int i = 0 ; i < N ; i++) {
-            numbers[i] = i+1;
+            numbers[i] = Integer.parseInt(inputStr[i]);
         }
 
-        int lt = 0, rt = 0;
-        int sum = 0, count = 0;
-        while(true) {
-            if(sum == N) {
-                count++;
-                sum -= numbers[lt++];
-            } else if(sum > N) {
-                sum -= numbers[lt++];
-            } else if(sum < N) {
-                if(rt == N-1) break;
-                sum += numbers[rt++];
+        int lt = 0;
+        int answer = 0;
+        int cnt = 0;
+
+        for(int rt = 0 ; rt < N ; rt++) {
+            if(numbers[rt] == 0) cnt++;
+            while(cnt > K) {
+                if(numbers[lt] == 0) cnt--;
+                lt++;
             }
+            answer = Math.max(answer, rt-lt+1);
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
 }

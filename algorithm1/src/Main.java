c3a11f6e32ca5d16153d7f46dd3dedd17afc6c12
EunDuk2 <dmsqls6789@naver.com>
2025-11-20 03:34:25 +0900

    인프런-4-2. 아나그램(해쉬)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,23 +5,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        char[] input = br.readLine().toCharArray();
+        char[] input1 = br.readLine().toCharArray();
+        char[] input2 = br.readLine().toCharArray();
 
-        Map<Character, Integer> map = new HashMap();
-
-        for(char c : input) {
-            map.put(c, map.getOrDefault(c, 0)+1);
+        Map<Character, Integer> map1 = new HashMap();
+        for(char c : input1) {
+            map1.put(c, map1.getOrDefault(c, 0)+1);
         }
 
-        int max = 0;
-        char maxChar = input[0];
-        for(char c : map.keySet()) {
-            if(map.get(c) > max) {
-                max = map.get(c);
-                maxChar = c;
+        Map<Character, Integer> map2 = new HashMap();
+        for(char c : input2) {
+            map2.put(c, map2.getOrDefault(c, 0)+1);
+        }
+
+        boolean isAnswer = true;
+        for(char c : map1.keySet()) {
+            if(!map2.containsKey(c)) {
+                isAnswer = false;
+                break;
+            }
+            if(map1.get(c) != map2.get(c)) {
+                isAnswer = false;
+                break;
             }
         }
-        System.out.println(maxChar);
+        if(isAnswer) System.out.println("YES");
+        else System.out.println("NO");
     }
 }

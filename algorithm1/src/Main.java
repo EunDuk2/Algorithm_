5f421e839920e05f8f0979a048dfcf74cadb047e
EunDuk2 <dmsqls6789@naver.com>
2025-11-19 22:06:43 +0900

    인프런-3-1. 두 배열 합치기

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,34 +5,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] strArr = br.readLine().split(" ");
-        int N = Integer.parseInt(strArr[0]);
-        int M = Integer.parseInt(strArr[1]);
+        int N = Integer.parseInt(br.readLine());
+        String[] firstArr = br.readLine().split(" ");
+        int M = Integer.parseInt(br.readLine());
+        String[] secondArr = br.readLine().split(" ");
 
-        int[][] arr = new int[M][N];
+        int[] first = new int[N];
+        int[] second = new int[M];
 
+        for(int i = 0 ; i < N ; i++) {
+            first[i] = Integer.parseInt(firstArr[i]);
+        }
         for(int i = 0 ; i < M ; i++) {
-            String[] input = br.readLine().split(" ");
-            for(int j = 0 ; j < N ; j++) {
-                arr[i][j] = Integer.parseInt(input[j]);
-            }
+            second[i] = Integer.parseInt(secondArr[i]);
         }
 
-        // 학생별로 돌면서, 자기보다 높은애들을 set에 넣고, 인원수 - set.size
-        int count = 0;
-        for(int i = 1 ; i <= N ; i++) {
-            Set<Integer> set = new HashSet();
-            for(int j = 0 ; j < M ; j++) {
-                for(int k = 0 ; k < N ; k++) {
-                    if(i == arr[j][k]) {
-                        break;
-                    } else {
-                        set.add(arr[j][k]);
-                    }
-                }
+        int fp = 0;
+        int sp = 0;
+
+        List<Integer> list = new ArrayList();
+
+        while(fp < N && sp < M) {
+            if(first[fp] < second[sp]) {
+                list.add(first[fp]);
+                fp++;
+            } else {
+                list.add(second[sp]);
+                sp++;
             }
-            count += N - set.size()-1;
         }
-        System.out.println(count);
+        if(fp == N) {
+            while(sp < M) {
+                list.add(second[sp++]);
+            }
+        } else {
+            while(fp < N) {
+                list.add(first[fp++]);
+            }
+        }
+        System.out.println(list);
     }
 }

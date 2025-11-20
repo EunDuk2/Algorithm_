418d2f0f8398b22876281cefaa7465ecf0f914c0
EunDuk2 <dmsqls6789@naver.com>
2025-11-20 22:01:11 +0900

    인프런-5-4. 후위식 연산(postfix)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,45 +5,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] board = new int[N][N];
+        char[] arr = br.readLine().toCharArray();
 
-        for(int i = 0 ; i < N ; i++) {
-            String[] input = br.readLine().split(" ");
-            for(int j = 0 ; j < N ; j++) {
-                board[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        int M = Integer.parseInt(br.readLine());
-        String[] input2 = br.readLine().split(" ");
-        int[] moves = new int[M];
-        for(int i = 0 ; i < M ; i++) {
-            moves[i] = Integer.parseInt(input2[i])-1;
-        }
-
-        int count = 0;
         Stack<Integer> stack = new Stack();
-        // moves의 인덱스를 j로 두고 i를 순회
-        for(int j : moves) {
-            for(int i = 0 ; i < N ; i++) {
-                // board[i][j]가 0이 아니면, 그 값을 스택에 담고, 0으로 수정
-                if(board[i][j] != 0) {
 
-                    // 스택 마지막 값이랑 같으면 안 넣고 하나 pop하고 +2
-                    if(!stack.isEmpty() && stack.peek() == board[i][j]) {
-                        stack.pop();
-                        count += 2;
-                    } else {
-                        stack.push(board[i][j]);
-                    }
-
-                    board[i][j] = 0;
-                    break;
+        for(char c : arr) {
+            if(c == '+' || c == '-' || c == '*' || c == '/') {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                if(c == '+') {
+                    stack.push(num1 + num2);
+                } else if(c == '-') {
+                    stack.push(num1 - num2);
+                } else if(c == '*') {
+                    stack.push(num1 * num2);
+                } else {
+                    stack.push(num1 / num2);
                 }
-
+            } else {
+                stack.push(c - '0');
             }
         }
-        System.out.println(count);
+        System.out.println(stack.pop());
     }
 }

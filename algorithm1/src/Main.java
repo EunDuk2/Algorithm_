1980ae3cb936c64c017c127ddf319288afcc79b3
EunDuk2 <dmsqls6789@naver.com>
2025-11-20 15:11:59 +0900

    프로그래머스-롤케이크 자르기

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,27 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] NK = br.readLine().split(" ");
-        int N = Integer.parseInt(NK[0]);
-        int K = Integer.parseInt(NK[1]);
-
-        String[] numbers = br.readLine().split(" ");
-
-        Map<String, Integer> map = new HashMap();
-
-        StringBuilder sb = new StringBuilder();
-        int lt = 0;
-        for(int rt = 0 ; rt < N ; rt++) {
-            map.put(numbers[rt], map.getOrDefault(numbers[rt], 0)+1);
-            if(rt-lt+1 == K) {
-                sb.append(map.size()).append(" ");
-                map.put(numbers[lt], map.get(numbers[lt])-1);
-                if(map.get(numbers[lt]) == 0) {
-                    map.remove(numbers[lt]);
-                }
-                lt++;
-            }
-        }
-        System.out.println(sb);
+        Solution s = new Solution();
+//        System.out.println(s.solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2}));
+        System.out.println(s.solution(new int[]{1, 2, 3, 1, 4}));
     }
 }
--- algorithm1/src/Solution.java
+++ algorithm1/src/Solution.java
@@ -1,17 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        Arrays.fill(answer, -1);
+    public int solution(int[] topping) {
+        int answer = 0;
 
-        Stack<Integer> stack = new Stack<>();
+        // map을 2개 선언해서, point가 한 칸 움직일 때마다, 각각 더하고 빼고
+        Map<Integer, Integer> leftMap = new HashMap();
+        Map<Integer, Integer> rightMap = new HashMap();
 
-        for(int i = 0 ; i < numbers.length ; i++) {
-            while(!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-                answer[stack.pop()] = numbers[i];
-            }
-            stack.push(i);
+        for(int i = 0 ; i < topping.length ; i++) {
+            rightMap.put(topping[i], rightMap.getOrDefault(topping[i], 0)+1);
+        }
+
+        // point를 하나 정해서 n만큼 이동
+        for(int point = 0 ; point < topping.length ; point++) {
+            // 2개의 map 개수 비교
+            if(leftMap.size() == rightMap.size()) answer++;
+
+            rightMap.put(topping[point], rightMap.get(topping[point])-1);
+            if(rightMap.get(topping[point]) == 0) rightMap.remove(topping[point]);
+
+            leftMap.put(topping[point], leftMap.getOrDefault(topping[point], 0)+1);
         }
         return answer;
     }

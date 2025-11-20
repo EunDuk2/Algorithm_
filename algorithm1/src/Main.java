34433456a0931b3d79406f8135fbe50e1c1217a0
EunDuk2 <dmsqls6789@naver.com>
2025-11-20 16:46:39 +0900

    인프런-4-4. 모든 아나그램 찾기

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,8 +5,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Solution s = new Solution();
-//        System.out.println(s.solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2}));
-        System.out.println(s.solution(new int[]{1, 2, 3, 1, 4}));
+        char[] input = br.readLine().toCharArray();
+        char[] ana = br.readLine().toCharArray();
+
+        // ana -> map
+        Map<Character, Integer> anaMap = new HashMap();
+        for(char c : ana) {
+            anaMap.put(c, anaMap.getOrDefault(c, 0)+1);
+        }
+
+        Map<Character, Integer> inputMap = new HashMap();
+        int count = 0;
+        int lt = 0;
+        for(int rt = 0 ; rt < input.length ; rt++) {
+            inputMap.put(input[rt], inputMap.getOrDefault(input[rt], 0)+1);
+
+            if(rt - lt + 1 == ana.length) {
+                if(inputMap.equals(anaMap)) {
+                    count++;
+                }
+
+                inputMap.put(input[lt], inputMap.get(input[lt]) - 1);
+                if(inputMap.get(input[lt]) == 0) inputMap.remove(input[lt]);
+                lt++;
+            }
+
+        }
+        System.out.println(count);
     }
 }

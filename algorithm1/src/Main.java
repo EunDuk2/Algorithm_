002a652ec904f015965d6ab45b10ef2d4164fdeb
EunDuk2 <dmsqls6789@naver.com>
2025-11-24 02:57:44 +0900

    인프런-6-1. 선택 정렬

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,71 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] board;
-    static int x, y;
-    static int d = 2; // 0-3 : 북동남서
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        board = new int[N][N];
-        x = 0;
-        y = 0;
-        
-        for(int i = N*N ; i > 0 ; i--) {
-            board[x][y] = i;
-            nextXy();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int[] a : board) {
-            for(int b : a) {
-                sb.append(b).append(" ");
-            }
-            sb.append("\n");
-        }
+        String[] inputArr = br.readLine().split(" ");
+        int[] input = new int[N];
         for(int i = 0 ; i < N ; i++) {
-            for(int j = 0 ; j < N ; j++) {
-                if(board[i][j] == M) {
-                    sb.append((i+1) + " " + (j+1));
-                }
+            input[i] = Integer.parseInt(inputArr[i]);
+        }
+
+        for(int i = 0 ; i < N ; i++) {
+            int minIndex = i;
+            for(int j = i+1 ; j < N ; j++) {
+                if(input[j] < input[minIndex]) minIndex = j;
             }
+            int temp = input[i];
+            input[i] = input[minIndex];
+            input[minIndex] = temp;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int n : input) {
+            sb.append(n).append(" ");
         }
         System.out.println(sb);
-    }
-    // 다음 위치 계산하고 못가면 방향바꾼 다음 위치로 반환
-    static void nextXy() {
-        if(d == 0) {
-            if(x-1 < 0 || board[x-1][y] != 0) {
-                d = 3;
-                y = y-1;
-            } else {
-                x = x-1;
-            }
-        } else if(d == 1) {
-            if(y+1 >= N || board[x][y+1] != 0) {
-                d = 0;
-                x = x-1;
-            } else {
-                y = y+1;
-            }
-        } else if(d == 2) {
-            if(x+1 >= N || board[x+1][y] != 0) {
-                d = 1;
-                y = y+1;
-            } else {
-                x = x+1;
-            }
-        } else if(d == 3) {
-            if(y-1 < 0 || board[x][y-1] != 0) {
-                d = 2;
-                x = x+1;
-            } else {
-                y = y-1;
-            }
-        }
     }
 }

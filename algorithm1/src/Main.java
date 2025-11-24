f8008c9b11b44999d8d9a4cf69f5397f65b891e8
EunDuk2 <dmsqls6789@naver.com>
2025-11-24 16:23:43 +0900

    백준-다각형의 면적 - 2166

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,23 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        long[] x = new long[N+1];
+        long[] y = new long[N+1];
 
-        String[] inputArr = br.readLine().split(" ");
-        int[] input = new int[N];
-        for(int i = 0 ; i < N ; i++) {
-            input[i] = Integer.parseInt(inputArr[i]);
+        for(int i=0; i<N; i++){
+            String[] arr = br.readLine().split(" ");
+            x[i] = Long.parseLong(arr[0]);
+            y[i] = Long.parseLong(arr[1]);
         }
-        int[] sorted = input.clone();
-        Arrays.sort(sorted);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < N ; i++) {
-            if(input[i] != sorted[i]) sb.append(i+1).append(" ");
+        // 마지막 점은 다시 처음으로 연결
+        x[N] = x[0];
+        y[N] = y[0];
+
+        long sum = 0;
+        for(int i=0; i<N; i++){
+            sum += x[i] * y[i+1] - y[i] * x[i+1];
         }
-        System.out.println(sb);
+
+        double area = Math.abs(sum) / 2.0;
+
+        System.out.printf("%.1f", area);
     }
 }

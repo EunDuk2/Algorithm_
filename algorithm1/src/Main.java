94643ac7c390471033801c8002496185474be13b
EunDuk2 <dmsqls6789@naver.com>
2025-11-25 18:00:58 +0900

    백준-N과 M (12) - 15666

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,34 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int M;
+
+    static List<Integer> numbers = new ArrayList();
+    static List<List<Integer>> doubleList = new ArrayList();
+    static List<Integer> temp = new ArrayList();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] NM = br.readLine().split(" ");
-        int N = Integer.parseInt(NM[0]);
-        int M = Integer.parseInt(NM[1]);
+        N = Integer.parseInt(NM[0]);
+        M = Integer.parseInt(NM[1]);
 
-        String[] inputStr = br.readLine().split(" ");
-        int[] arr = new int[N];
-        for(int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(inputStr[i]);
+        String[] inputNumbers = br.readLine().split(" ");
+
+        Set<Integer> set = new TreeSet();
+
+        for(int i = 0 ; i < inputNumbers.length ; i++) {
+            set.add(Integer.parseInt(inputNumbers[i]));
         }
-        Arrays.sort(arr);
-        int answer = -1;
+        N = set.size();
 
-        int lt = 0;
-        int rt = arr.length-1;
+        for(int n : set) {
+            numbers.add(n);
+        }
 
-        while(lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if(arr[mid] == M) {
-                answer = mid+1;
-                break;
+        dfs(0);
+
+        StringBuilder sb = new StringBuilder();
+        for(List<Integer> list : doubleList) {
+            for(int n : list) {
+                sb.append(n).append(" ");
             }
-            else if(arr[mid] > M) rt = mid-1;
-            else lt = mid+1;
+            sb.append("\n");
         }
-
-        System.out.println(answer);
+        System.out.println(sb);
+    }
+    static void dfs(int start) {
+        if(temp.size() == M) {
+            doubleList.add(new ArrayList(temp));
+            return;
+        }
+        for(int i = start ; i < N ; i++) {
+            temp.add(numbers.get(i));
+            dfs(i);
+            temp.remove(temp.size()-1);
+        }
     }
 }

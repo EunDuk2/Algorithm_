cedc16aaae73600ddb250fe07952a30b5d7073a9
EunDuk2 <dmsqls6789@naver.com>
2025-11-25 16:00:05 +0900

    인프런-6-8. 이분검색

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,25 +5,31 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
 
-        List<int[]> list = new ArrayList();
+        String[] inputStr = br.readLine().split(" ");
+        int[] arr = new int[N];
         for(int i = 0 ; i < N ; i++) {
-            String[] input = br.readLine().split(" ");
-            list.add(new int[2]);
-            list.get(i)[0] = Integer.parseInt(input[0]);
-            list.get(i)[1] = Integer.parseInt(input[1]);
+            arr[i] = Integer.parseInt(inputStr[i]);
+        }
+        Arrays.sort(arr);
+        int answer = -1;
+
+        int lt = 0;
+        int rt = arr.length-1;
+
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(arr[mid] == M) {
+                answer = mid+1;
+                break;
+            }
+            else if(arr[mid] > M) rt = mid-1;
+            else lt = mid+1;
         }
 
-        list.sort((a, b) -> {
-           int compare1 = a[0] - b[0];
-           if(compare1 != 0) return compare1;
-           int compare2 = a[1] - b[1];
-           return compare2;
-        });
-
-        for(int[] n : list) {
-            System.out.println(n[0] + " " + n[1]);
-        }
+        System.out.println(answer);
     }
 }

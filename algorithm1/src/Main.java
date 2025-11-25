f14bbe9c8dadf57cdabec5af0ebf46d1a6664632
EunDuk2 <dmsqls6789@naver.com>
2025-11-25 18:34:00 +0900

    인프런-6-9. 뮤직비디오(결정알고리즘)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,53 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-
-    static List<Integer> numbers = new ArrayList();
-    static List<List<Integer>> doubleList = new ArrayList();
-    static List<Integer> temp = new ArrayList();
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] NM = br.readLine().split(" ");
-        N = Integer.parseInt(NM[0]);
-        M = Integer.parseInt(NM[1]);
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
 
-        String[] inputNumbers = br.readLine().split(" ");
+        String[] input = br.readLine().split(" ");
 
-        Set<Integer> set = new TreeSet();
-
-        for(int i = 0 ; i < inputNumbers.length ; i++) {
-            set.add(Integer.parseInt(inputNumbers[i]));
-        }
-        N = set.size();
-
-        for(int n : set) {
-            numbers.add(n);
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(input[i]);
         }
 
-        dfs(0);
-
-        StringBuilder sb = new StringBuilder();
-        for(List<Integer> list : doubleList) {
-            for(int n : list) {
-                sb.append(n).append(" ");
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        int answer = 0;
+        while(lt <= rt) {
+            int mid = (lt+rt) / 2;
+            if(count(arr, mid) <= M) {
+                answer = mid;
+                rt = mid-1;
+            } else {
+                lt = mid+1;
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(answer);
     }
-    static void dfs(int start) {
-        if(temp.size() == M) {
-            doubleList.add(new ArrayList(temp));
-            return;
+    // 배열이랑 용량받고, 몇 개 필요한지 반환
+    static int count(int[] arr, int capa) {
+        int count = 1;
+        int sum = 0;
+        for(int n : arr) {
+            if(sum+n > capa) {
+                count++;
+                sum = n;
+            } else {
+                sum += n;
+            }
         }
-        for(int i = start ; i < N ; i++) {
-            temp.add(numbers.get(i));
-            dfs(i);
-            temp.remove(temp.size()-1);
-        }
+        return count;
     }
 }

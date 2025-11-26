b40aae288835663674285b155a64548e20dc35f5
EunDuk2 <dmsqls6789@naver.com>
2025-11-26 17:24:39 +0900

    인프런-6-10. 마구간 정하기(결정알고리즘)

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,27 +5,45 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // (는 스택에 넣고
-        // )는 스택에 (로 제거하고, 스택에 (가 없으면 count++
-        // 끝나고는 스택에 남은 ( 개수 count++
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int C = Integer.parseInt(NM[1]);
 
-        char[] input = br.readLine().toCharArray();
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr);
 
-        int count = 0;
-        Stack<Character> stack = new Stack();
-        for(char c : input) {
-            if(c == '(') {
-                stack.push(c);
+        int lt = 1;
+        int rt = arr[N-1] - arr[0];
+        int answer = 0;
+
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+            int count = count(arr, mid);
+
+            if(count >= C) {      // mid 거리로 설치 가능
+                answer = mid;    // 후보 업데이트
+                lt = mid + 1;    // 더 큰 거리 탐색
             } else {
-                if(!stack.isEmpty()) {
-                    stack.pop();
-                } else {
-                    count++;
-                }
+                rt = mid - 1;    // 더 작은 거리 탐색
             }
         }
-        count += stack.size();
 
-        System.out.println(count);
+        System.out.println(answer);
+    }
+
+    static int count(int[] arr, int dist) {
+        int count = 1;
+        int last = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] - last >= dist) {
+                count++;
+                last = arr[i];
+            }
+        }
+        return count;
     }
 }

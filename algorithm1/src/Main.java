2f3f40034e9c4a3b81bd49f645ee2b2121f78370
EunDuk2 <dmsqls6789@naver.com>
2025-11-27 15:48:20 +0900

    백준-소가 길을 건너간 이유 5 - 14465

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -5,29 +5,35 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] NK = br.readLine().split(" ");
-        int N = Integer.parseInt(NK[0]);
-        int K = Integer.parseInt(NK[1]);
+        String[] NKB = br.readLine().split(" ");
+        int N = Integer.parseInt(NKB[0]);
+        int K = Integer.parseInt(NKB[1]);
+        int B = Integer.parseInt(NKB[2]);
 
-        String[] input = br.readLine().split(" ");
         int[] numbers = new int[N];
-        for(int i = 0 ; i < N ; i++) {
-            numbers[i] = Integer.parseInt(input[i]);
+        for(int i = 0 ; i < B ; i++) {
+            int index = Integer.parseInt(br.readLine())-1;
+            numbers[index] = 1;
         }
 
-        int lt = 0;
-        int k = 0;
-        int maxLength = 0;
-        for(int rt = 0 ; rt < N ; rt++) {
-            if(numbers[rt] == 0) {
-                k++;
-                while(k>K) {
-                    if(numbers[lt] == 0) k--;
-                    lt++;
+        int answer = -1;
+        question: for(int i = 0 ; i <= B ; i++) {
+            int lt = 0;
+            int currentK = 0;
+            for(int rt = 0 ; rt < N ; rt++) {
+                if(numbers[rt] == 1) {
+                    currentK++;
+                    while(currentK > i) {
+                        if(numbers[lt] == 1) currentK--;
+                        lt++;
+                    }
+                }
+                if((rt-lt+1) == K) {
+                    answer = i;
+                    break question;
                 }
             }
-            maxLength = Math.max(maxLength, (rt-lt+1));
         }
-        System.out.println(maxLength);
+        System.out.println(answer);
     }
 }

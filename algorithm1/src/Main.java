08c08879d22d5e762ae7c2e0a46eb835c09db9dc
EunDuk2 <dmsqls6789@naver.com>
2025-12-02 16:06:43 +0900

    백준-부분수열의 합 - 1182

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,41 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int S;
+    static int[] numbers;
+    static int sum;
+    static int count = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-        int M = Integer.parseInt(br.readLine());
+        String[] NS = br.readLine().split(" ");
+        N = Integer.parseInt(NS[0]);
+        S = Integer.parseInt(NS[1]);
 
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
-
-        for (char c : str.toCharArray()) left.push(c);
-
-        while (M-- > 0) {
-            String cmd = br.readLine();
-            switch (cmd.charAt(0)) {
-                case 'L':
-                    if (!left.isEmpty()) right.push(left.pop());
-                    break;
-
-                case 'D':
-                    if (!right.isEmpty()) left.push(right.pop());
-                    break;
-
-                case 'B':
-                    if (!left.isEmpty()) left.pop();
-                    break;
-
-                case 'P':
-                    left.push(cmd.charAt(2));
-                    break;
-            }
+        String[] input = br.readLine().split(" ");
+        numbers = new int[N];
+        for(int i = 0 ; i < N ; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (char c : left) sb.append(c);
-        while (!right.isEmpty()) sb.append(right.pop());
-        System.out.println(sb);
+        dfs(0);
+
+        System.out.println(count);
+    }
+    static void dfs(int start) {
+        if(start == N) {
+
+            return;
+        }
+        for(int i = start ; i < N ; i++) {
+            sum += numbers[i];
+            if(sum == S) count++;
+            dfs(i+1);
+            sum -= numbers[i];
+        }
     }
 }

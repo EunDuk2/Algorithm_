ed59da44d6fb6af5d91c244a28b47336bebf476c
EunDuk2 <dmsqls6789@naver.com>
2025-12-03 20:12:00 +0900

    백준-DFS와 BFS - 1260

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,29 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static List<List<Integer>> doubleList = new ArrayList();
+    static int N, M, V;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        int K = Integer.parseInt(br.readLine());
+        String[] NMV = br.readLine().split(" ");
+        N = Integer.parseInt(NMV[0]);
+        M = Integer.parseInt(NMV[1]);
+        V = Integer.parseInt(NMV[2]);
 
-        int[] numbers = new int[N];
-        for(int i = 0 ; i < N ; i++) {
-            numbers[i] = Integer.parseInt(input[i]);
+        visited = new boolean[N+1];
+
+        for(int i = 0 ; i < N+1 ; i++) {
+            doubleList.add(new ArrayList());
         }
 
-        long count = 0;
-        long sum = 0;
-        int lt = 0;
-        for(int rt = 0 ; rt < N ; rt++) {
-            sum += numbers[rt];
-            while(sum > K) {
-                count += N-rt;
-                sum -= numbers[lt];
-                lt++;
+        for(int i = 0 ; i < M ; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            doubleList.get(a).add(b);
+            doubleList.get(b).add(a);
+        }
+
+        for(List<Integer> list : doubleList) {
+            list.sort(Comparator.naturalOrder());
+        }
+
+        dfs(V);
+
+        visited = new boolean[N+1];
+        sb.append("\n");
+        bfs(V);
+
+        System.out.println(sb);
+
+    }
+    static void dfs(int start) {
+        visited[start] = true;
+        sb.append(start).append(" ");
+
+        for(int n : doubleList.get(start)) {
+            if(!visited[n]) dfs(n);
+        }
+    }
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList();
+        q.add(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()) {
+            int current = q.poll();
+            sb.append(current).append(" ");
+
+            for(int n : doubleList.get(current)) {
+                if(!visited[n]) {
+                    q.add(n);
+                    visited[n] = true;
+                }
             }
         }
-        System.out.println(count);
     }
 }

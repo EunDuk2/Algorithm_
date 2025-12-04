a8f947b351c751905e42b624aa7ebcd151cbfde0
EunDuk2 <dmsqls6789@naver.com>
2025-12-04 15:42:28 +0900

    백준-부분수열의 합-14225

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,63 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static List<List<Integer>> doubleList = new ArrayList();
-    static int[] dist;
-    static boolean[] visited;
+    static int N;
+    static int[] numbers;
+    static int[] sums;
+    static int sum = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] NM = br.readLine().split(" ");
-        N = Integer.parseInt(NM[0]);
-        M = Integer.parseInt(NM[1]);
-
-        dist = new int[N+1];
-        visited = new boolean[N+1];
-
-        for(int i = 0 ; i < N+1 ; i++) {
-            doubleList.add(new ArrayList());
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
+        String[] input = br.readLine().split(" ");
+        for(int i = 0 ; i < N ; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
         }
-        for(int i = 0 ; i < M ; i++) {
-            String[] input = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            int b = Integer.parseInt(input[1]);
-            doubleList.get(a).add(b);
-            doubleList.get(a).sort(Comparator.naturalOrder());
-            doubleList.get(b).add(a);
-            doubleList.get(b).sort(Comparator.naturalOrder());
-        }
+        int max = Arrays.stream(numbers).sum();
+        sums = new int[max+1];
 
+        dfs(0);
 
-        bfs(1);
-
-        int max = Arrays.stream(dist).max().getAsInt();
-        int order = -1;
-        int count = 0;
-
-        for(int i = 0 ; i < dist.length ; i++) {
-            if(dist[i] == max) {
-                if(order == -1) order = i;
-                count++;
+        int answer = max+1;
+        for(int i = 1 ; i < sums.length ; i++) {
+            if(sums[i] == 0) {
+                answer = i;
+                break;
             }
         }
-        System.out.println(order + " " + max + " " + count);
+        System.out.println(answer);
     }
-    static void bfs(int start) {
-        Queue<Integer> q = new LinkedList();
-        q.add(start);
-        visited[start] = true;
 
-        while(!q.isEmpty()) {
-            int current = q.poll();
-            for(int next : doubleList.get(current)) {
-                if(!visited[next]) {
-                    q.add(next);
-                    visited[next] = true;
-                    dist[next] = dist[current]+1;
-                }
-            }
+    static void dfs(int start) {
+        for(int i = start ; i < N ; i++) {
+            sum += numbers[i];
+            sums[sum] = 1;
+            dfs(i+1);
+            sum -= numbers[i];
         }
     }
 }

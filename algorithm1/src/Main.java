2639492a6f7fb375b3a8c5beaa1847941a7faef1
EunDuk2 <dmsqls6789@naver.com>
2025-12-10 01:38:10 +0900

    A → B - 16953

--- algorithm1/src/Main.java
+++ algorithm1/src/Main.java
@@ -2,60 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static int N;
-    static int[][] graph;
-    static int[][] dist;
-    static final int INF = Integer.MAX_VALUE;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] input = br.readLine().split(" ");
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+
         int count = 1;
-        while(true) {
-            N = Integer.parseInt(br.readLine());
-            if(N == 0) break;
 
-            graph = new int[N][N];
-            dist = new int[N][N];
-            for(int[] d : dist) {
-                Arrays.fill(d, INF);
+        while (B > A) {
+            if (B % 10 == 1) {
+                B /= 10;
+            } else if (B % 2 == 0) {
+                B /= 2;
+            } else {
+                System.out.println(-1);
+                return;
             }
-
-            for(int i = 0 ; i < N ; i++) {
-                String[] input = br.readLine().split(" ");
-
-                for(int j = 0 ; j < N ; j++) {
-                    graph[i][j] = Integer.parseInt(input[j]);
-                }
-            }
-
-            dijkstra(0, 0);
-            System.out.println("Problem " + count++ + ": " + dist[N-1][N-1]);
+            count++;
         }
-    }
-    static void dijkstra(int x, int y) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> graph[a[0]][a[1]] - graph[b[0]][b[1]]);
-        pq.add(new int[]{x, y});
-        dist[x][y] = graph[x][y];
 
-        while(!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int cx = current[0];
-            int cy = current[1];
-
-            for(int i = 0 ; i < 4 ; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if(nx > -1 && nx < N && ny > -1 && ny < N) {
-                    if(dist[nx][ny] > dist[cx][cy] + graph[nx][ny]) {
-                        pq.add(new int[]{nx, ny});
-                        dist[nx][ny] = dist[cx][cy] + graph[nx][ny];
-                    }
-                }
-            }
-        }
+        System.out.println(B == A ? count : -1);
     }
 }
